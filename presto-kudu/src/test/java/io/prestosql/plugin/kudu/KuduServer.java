003d46a8c564b3746b7db655443c086196f3708d
praveenkrishna <praveenkrishna@tutanota.com>
2020-03-16 16:53:38 +0530

    Test

--- presto-kudu/src/test/java/io/prestosql/plugin/kudu/KuduServer.java
+++ presto-kudu/src/test/java/io/prestosql/plugin/kudu/KuduServer.java
@@ -14,44 +14,62 @@
  */
 package io.prestosql.plugin.kudu;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.net.HostAndPort;
 import io.airlift.log.Logger;
-import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.GenericContainer;
 
 import java.io.Closeable;
-import java.io.File;
-
-import static com.google.common.io.Resources.getResource;
+import java.util.List;
 
 public class KuduServer
         implements Closeable
 {
     private static Logger log = Logger.get(KuduServer.class);
 
-    private static final int KUDU_MASTER_PORT = 7051;
-    private static final int KUDU_TSERVER_PORT = 7050;
+    private static final List<Integer> KUDU_MASTER_PORTS = ImmutableList.of(7051, 7151, 7251);
+    private static final List<Integer> KUDU_TSERVER_PORTS = ImmutableList.of(7050, 7150, 7250);
 
-    private final DockerComposeContainer<?> dockerComposeContainer;
+    private final List<GenericContainer<?>> masterContainers;
+    private final List<GenericContainer<?>> tServerContainers;
 
     public KuduServer()
     {
         log.info("Starting kudu...");
-        dockerComposeContainer = new DockerComposeContainer(new File(getResource("docker-compose.yml").getFile()))
-                .withExposedService("kudu-master_1", KUDU_MASTER_PORT)
-                .withExposedService("kudu-tserver_1", KUDU_TSERVER_PORT);
-        dockerComposeContainer.start();
+        ImmutableList.Builder<GenericContainer<?>> masterBuilder = ImmutableList.builder();
+        ImmutableList.Builder<GenericContainer<?>> tServerBuilder = ImmutableList.builder();
+        String address =  DockerClientFactory.instance().dockerHostIpAddress();
+        String kuduMasters = address + ":7051," + address + ":7151," + address + ":7251";
+        for (Integer masterPort : KUDU_MASTER_PORTS) {
+            masterBuilder.add(new GenericContainer<>("apache/kudu-1.10.0")
+                    .withExposedPorts(masterPort)
+                    .withCommand("master")
+                    .withEnv("KUDU_MASTERS", kuduMasters));
+        }
+        masterContainers = masterBuilder.build();
+        for (Integer slavePort : KUDU_TSERVER_PORTS) {
+            tServerBuilder.add(new GenericContainer<>("apache/kudu-1.10.0")
+                    .withExposedPorts(slavePort)
+                    .withCommand("tserver")
+                    .withEnv("KUDU_MASTERS", kuduMasters));
+        }
+        tServerContainers = tServerBuilder.build();
+        masterContainers.forEach(GenericContainer::start);
+        tServerContainers.forEach(GenericContainer::start);
     }
 
     public HostAndPort getMasterAddress()
     {
         return HostAndPort.fromParts(
-                dockerComposeContainer.getServiceHost("kudu-master_1", KUDU_MASTER_PORT),
-                dockerComposeContainer.getServicePort("kudu-master_1", KUDU_MASTER_PORT));
+                masterContainers.get(0).getContainerIpAddress(),
+                masterContainers.get(0).getMappedPort(7051));
     }
 
     @Override
     public void close()
     {
-        dockerComposeContainer.close();
+        masterContainers.forEach(GenericContainer::stop);
+        tServerContainers.forEach(GenericContainer::stop);
     }
 }
